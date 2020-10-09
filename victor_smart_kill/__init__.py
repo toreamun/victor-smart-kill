f021294ffb4479c6cef349ab9b443a3926a90ae9
tore <tore@amundsen.org>
2020-10-09 23:04:23 +0200

    sort imports on save

--- victor_smart_kill/__init__.py
+++ victor_smart_kill/__init__.py
@@ -1,13 +1,5 @@
 """Victor Smart Kill API module."""
 # flake8: noqa: F401
-from ._client import VictorAsyncClient
-
-from ._models import (
-    Activity,
-    MobileApp,
-    Operator,
-    Profile,
-    Trap,
-    User,
-)
 from ._api import VictorApi
+from ._client import VictorAsyncClient
+from ._models import Activity, MobileApp, Operator, Profile, Trap, User
