197150041be5eb85714d0709c3a5963e7a574d24
tore <tore@amundsen.org>
2020-11-05 23:30:32 +0100

    Flake8 changes and black formatting.

--- listtraps.py
+++ listtraps.py
@@ -1,79 +1,137 @@
 #!/usr/bin/env python3.8
 
-"""list the traps and status"""
+"""List the traps and status."""
 
-import os, sys, string, time, logging, argparse
+import argparse
+import asyncio
+import logging
+import os
+import sys
+
+from victor_smart_kill import VictorApi, VictorAsyncClient
 
 _version = "0.1"
 
 try:
-  import config
+    import config
 except ImportError:
-  logging.warning("no config file present. Copy sample_config.py to config.py and add your username and password.")
-  sys.exit(1)
+    logging.warning(
+        (
+            "No config file present. Copy sample_config.py to config.py"
+            " and add your username and password."
+        )
+    )
+    sys.exit(1)
 
-import asyncio
-from victor_smart_kill import VictorApi, VictorAsyncClient
 
 async def start():
-  async with VictorAsyncClient(config.username, config.password) as client:
-    if os.path.exists(".token"):
-      client._token = open(".token").read()
+    """Print trap details."""
+    async with VictorAsyncClient(config.username, config.password) as client:
+        if os.path.exists(".token"):
+            client._token = open(".token").read()
 
-    api = VictorApi(client)
+        api = VictorApi(client)
 
-    if client._token:
-      open(".token", "w").write(client._token)
-    
-    traps = await api.get_traps()
+        if client._token:
+            open(".token", "w").write(client._token)
 
-    traps.sort(key=lambda x: x.name)
-    for n, trap in enumerate(traps):
-      print ("%2s %-30s %10s %3s %3s %3s" % (n, trap.name, trap.ssid, trap.status, trap.trapstatistics.battery_level, trap.trapstatistics.kills_present))
+        traps = await api.get_traps()
 
-    if 0:
-      history = await api.get_trap_history(traps[0].id)
-      for act in history:
-        print (act.sequence_number, act.time_stamp, act.activity_type, act.activity_type_text, act.battery_level, act.is_rat_kill)
+        traps.sort(key=lambda x: x.name)
+        for n, trap in enumerate(traps):
+            print(
+                "%2s %-30s %10s %3s %3s %3s"
+                % (
+                    n,
+                    trap.name,
+                    trap.ssid,
+                    trap.status,
+                    trap.trapstatistics.battery_level,
+                    trap.trapstatistics.kills_present,
+                )
+            )
+
+        if 0:
+            history = await api.get_trap_history(traps[0].id)
+            for act in history:
+                print(
+                    act.sequence_number,
+                    act.time_stamp,
+                    act.activity_type,
+                    act.activity_type_text,
+                    act.battery_level,
+                    act.is_rat_kill,
+                )
+
 
 def test():
-  logging.warn("Testing")
+    """Print test info."""
+    logging.warn("Testing")
+
 
 def parse_args(argv):
-  parser = argparse.ArgumentParser(
-    formatter_class=argparse.ArgumentDefaultsHelpFormatter,
-    description=__doc__)
+    """Parse command arguments."""
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter, description=__doc__
+    )
 
-  parser.add_argument("-t", "--test", dest="test_flag", 
-                    default=False,
-                    action="store_true",
-                    help="Run test function")
-  parser.add_argument("--log-level", type=str,
-                      choices=["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"],
-                      help="Desired console log level")
-  parser.add_argument("-d", "--debug", dest="log_level", action="store_const",
-                      const="DEBUG",
-                      help="Activate debugging")
-  parser.add_argument("-q", "--quiet", dest="log_level", action="store_const",
-                      const="CRITICAL",
-                      help="Quite mode")
-  #parser.add_argument("files", type=str, nargs='+')
+    parser.add_argument(
+        "-t",
+        "--test",
+        dest="test_flag",
+        default=False,
+        action="store_true",
+        help="Run test function",
+    )
+    parser.add_argument(
+        "--log-level",
+        type=str,
+        choices=["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"],
+        help="Desired console log level",
+    )
+    parser.add_argument(
+        "-d",
+        "--debug",
+        dest="log_level",
+        action="store_const",
+        const="DEBUG",
+        help="Activate debugging",
+    )
+    parser.add_argument(
+        "-q",
+        "--quiet",
+        dest="log_level",
+        action="store_const",
+        const="CRITICAL",
+        help="Quite mode",
+    )
+    # parser.add_argument("files", type=str, nargs='+')
 
-  args = parser.parse_args(argv[1:])
+    args = parser.parse_args(argv[1:])
 
-  return parser, args
+    return parser, args
+
 
 def main(argv, stdout, environ):
-  if sys.version_info < (3, 0): reload(sys); sys.setdefaultencoding('utf8')
+    """Program main entry point."""
+    if sys.version_info < (3, 0):
+        reload(sys)
+        sys.setdefaultencoding("utf8")
 
-  parser, args = parse_args(argv)
+    parser, args = parse_args(argv)
 
-  logging.basicConfig(format="[%(asctime)s] %(levelname)-8s %(message)s", 
-                    datefmt="%m/%d %H:%M:%S", level=args.log_level)
+    logging.basicConfig(
+        format="[%(asctime)s] %(levelname)-8s %(message)s",
+        datefmt="%m/%d %H:%M:%S",
+        level=args.log_level,
+    )
 
-  if args.test_flag:  test();   return
+    if args.test_flag:
+        test()
+        return
 
-  asyncio.run(start())
+    asyncio.run(start())
+
 
 if __name__ == "__main__":
-  main(sys.argv, sys.stdout, os.environ)
+    main(sys.argv, sys.stdout, os.environ)
