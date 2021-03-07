ab1f1babf701a5f72a0e110de1151c5c8a0daaf8
tore <tore@amundsen.org>
2021-03-07 22:26:03 +0100

    Calendar version schema and upgraded dependencies

--- setup.py
+++ setup.py
@@ -6,10 +6,10 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="victor_smart_kill",
-    version="0.1.0",
+    version="2021.3.0",
     author="Tore Amundsen",
     author_email="tore@amundsen.org",
-    description="A simple async Python wrapper for Victor Smart-Kill API.",
+    description="A simple unofficial async Python wrapper for Victor Smart-Kill API.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/toreamun/victor-smart-kill",
@@ -28,7 +28,7 @@ setuptools.setup(
     ],
     python_requires=">=3.6",
     install_requires=[
-        "httpx>=0.16.*",
+        "httpx>=0.17.*",
         "marshmallow>=3.8",
         "marshmallow-dataclass[enum,union]>=8.1",
     ],
